d83af2f9c65bd438ef3b5a9d777424af2be9aa14
ajlopez <webmaster@ajlopez.com>
2013-09-25 18:52:34 -0300

    List isList first tests

--- test/list.js
+++ test/list.js
@@ -21,3 +21,12 @@ exports['create list with two elements'] = function (test) {
 }exports['create list with nested element'] = function (test) {    var lst = list.createList(1, list.createList(2));    test.equal(lst.first(), 1);    test.equal(lst.rest().asString(), "((2))");    test.equal(lst.asString(), "(1 (2))");
 }exports['create list with two nils'] = function (test) {    var lst = list.createList(null, null);    test.equal(lst.first(), null);    test.equal(lst.asString(), "(nil nil)");
 }exports['create empty list'] = function (test) {    var lst = list.createList();    test.equal(lst.first(), null);    test.equal(lst.rest(), null);    test.equal(lst.asString(), "()");}
+exports['is list'] = function (test) {
+    var emptylst = list.createList();
+    var lst = list.createList(1, 2);
+    test.ok(list.isList(emptylst));
+    test.ok(list.isList(lst));
+    test.equal(list.isList(null), false);
+    test.equal(list.isList(12), false);
+    test.equal(list.isList('foo'), false);
+}
