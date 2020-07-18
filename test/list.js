4806d2647d079ee5c6faf20e2fd02c91c7fcc9fe
ajlopez <webmaster@ajlopez.com>
2020-07-18 10:53:16 -0300

    List test minor refactor

--- test/list.js
+++ test/list.js
@@ -1,6 +1,6 @@
 
-var list = require('../lib/list');
-var symbol = require('../lib/symbol');
+const list = require('../lib/list');
+const symbol = require('../lib/symbol');
 
 exports['create list function'] = function (test) {
     test.ok(list.createList);
@@ -8,45 +8,52 @@ exports['create list function'] = function (test) {
 }
 
 exports['create list with one element'] = function (test) {
-    var lst = list.createList(1);
+    const lst = list.createList(1);
+    
     test.equal(lst.first(), 1);
     test.equal(lst.rest(), null);
     test.equal(lst.asString(), "(1)");
 }
 
 exports['create list with two elements'] = function (test) {
-    var lst = list.createList(1, 2);
+    const lst = list.createList(1, 2);
+    
     test.equal(lst.first(), 1);
     test.equal(lst.rest().asString(), "(2)");
     test.equal(lst.asString(), "(1 2)");
 }
 
 exports['create list with nested element'] = function (test) {
-    var lst = list.createList(1, list.createList(2));
+    const lst = list.createList(1, list.createList(2));
+    
     test.equal(lst.first(), 1);
     test.equal(lst.rest().asString(), "((2))");
     test.equal(lst.asString(), "(1 (2))");
 }
 
 exports['create list with two nils'] = function (test) {
-    var lst = list.createList(null, null);
+    const lst = list.createList(null, null);
+    
     test.equal(lst.first(), null);
     test.equal(lst.asString(), "(nil nil)");
 }
 
 exports['create empty list'] = function (test) {
-    var lst = list.createList();
+    const lst = list.createList();
+    
     test.equal(lst.first(), null);
     test.equal(lst.rest(), null);
     test.equal(lst.asString(), "()");
 }
 
 exports['is list'] = function (test) {
-    var emptylst = list.createList();
-    var lst = list.createList(1, 2);
+    const emptylst = list.createList();
+    const lst = list.createList(1, 2);
+    
     test.ok(list.isList(emptylst));
     test.ok(list.isList(lst));
     test.equal(list.isList(null), false);
     test.equal(list.isList(12), false);
     test.equal(list.isList('foo'), false);
 }
+
